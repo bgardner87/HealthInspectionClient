f221929f37059e64ca7cb6ce4a4096a5ed87c584
Will Hawkins <whh8b@virginia.edu>
2018-05-14 20:58:02 -0400

    Updates for workshoppers.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 README
+
+Before you can use this client, you need to install the API. 
+
+`go get github.com/CincyGolangMeetup/HealthInspection/`
--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"github.com/CincyGolangMeetup/HealthInspection"
+	"fmt"
+	"github.com/CincyGolangMeetup/HealthInspection/Inspection/Query"
 )
 
 func main() {
-	q := healthinspection.NewQuery()
+	q := query.NewQuery()
 	q = q.QueryWithBusinessName("NAME")
 	q = q.QueryWithAddress("ADDRESS")
 	q = q.QueryWithCity("CITY")
 	q = q.QueryWithState("ST")
 	q.Print()
-	for _, r := range *q.Execute() {
-		r.Print()
+	results, err := q.Execute()
+	if (err != nil) {
+		fmt.Printf("%v\n", err);
+		return;
+	}
+	for _, result := range *results {
+		result.Print()
 	}
 }
